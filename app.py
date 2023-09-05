1396d91ad33752b1dfe4626a61fff683bc2d71da
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-09-05 17:35:23 +0300

    quick update

--- app.py
+++ app.py
@@ -11,19 +11,6 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/validate_password", methods=["POST"])
-# def validate_password():
-#     pwd = request.form["user_input"]
-#     pwd_condition = password_advisor(pwd)[0]
-#     pwd_issues = password_advisor(pwd)[1]
-
-#     # return render_template("feedback.html", pwd_condition=pwd_condition, pwd_issues=pwd_issues.split("\n"))
-#     if len(pwd_issues) == 0:
-#             return jsonify({"message": "Password is strong!"}), 200
-#     else:
-#         return jsonify({"message": "Password is weak!", "issues": pwd_issues}), 400
-
-
 @app.route("/validate_password", methods=["POST"])
 def validate_password():
     try:
