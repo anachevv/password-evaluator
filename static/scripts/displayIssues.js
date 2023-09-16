e0d821b65641bab8ec2cb107b15bf685946f4105
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2023-09-16 21:22:44 +0300

    modified

--- static/scripts/displayIssues.js
+++ static/scripts/displayIssues.js
@@ -1,39 +1,48 @@
-function validatePassword() {
-    const password = document.getElementById('user_input').value;
+document.addEventListener('DOMContentLoaded', function () {
+    const userInputElement = document.getElementById('user_input');
     const validationMessages = document.getElementById('password-validation-result');
 
-    if (password.trim() === "") {
-        validationMessages.textContent = ''; // Clear any previous validation messages
-        validationMessages.style.paddingTop = "0px";
-        return;
-    }
+    userInputElement.addEventListener('input', validatePassword);
 
-    fetch('/validate_password', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password }),
-    })
-    .then(response => response.json())
-    .then(data => {
+    function validatePassword() {
+        const password = userInputElement.value.trim();
 
-        if (data.message === "Password is strong!") {
-            validationMessages.textContent = data.message;
-            validationMessages.style.color = "green";
-            validationMessages.style.paddingTop = "20px";
-        } else if (data.message === "Password is weak! It is highly recommended to strengthen your password.") {
-            validationMessages.innerHTML = ''; // Clear previous validation messages
-            const issuesArray = data.issues.split('\n');
-            issuesArray.forEach(issue => {
-                const message = document.createElement('p');
-                message.textContent = issue;
-                validationMessages.appendChild(message);
-            });
-            validationMessages.style.color = "salmon";
+        if (password === "") {
+            validationMessages.textContent = '';
+            validationMessages.style.paddingTop = "0px";
+            return;
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+
+        fetch('/validate_password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ password }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (password === userInputElement.value.trim()) {
+                if (data.message === "Password is strong!") {
+                    validationMessages.textContent = data.message;
+                    validationMessages.style.color = "green";
+                    validationMessages.style.paddingTop = "20px";
+                } else if (data.message === "Password is weak! It is highly recommended to strengthen your password.") {
+                    validationMessages.innerHTML = '';
+                    const issuesArray = data.issues.split('\n');
+                    issuesArray.forEach(issue => {
+                        const message = document.createElement('p');
+                        message.textContent = issue;
+                        validationMessages.appendChild(message);
+                    });
+                    validationMessages.style.color = "salmon";
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+});
+
+
